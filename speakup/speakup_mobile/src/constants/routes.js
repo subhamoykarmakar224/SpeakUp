25312c81a09cb03337f682a61695d3b3cb6d8c24
Arijit Kumar Roy <arijitroy003@gmail.com>
2021-06-21 22:15:21 +0530

    bugfix

--- speakup/speakup_mobile/src/constants/routes.js
+++ speakup/speakup_mobile/src/constants/routes.js
@@ -5,4 +5,5 @@ export default ROUTES = {
   Verify: "verify",
   Dashboard: "dashboard",
   Search: "search",
+  Task: "task"
 };
--- speakup/speakup_mobile/src/screens/TaskScreen.js
+++ speakup/speakup_mobile/src/screens/TaskScreen.js
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {  StyleSheet, Text, View } from "react-native";
 
 const TaskScreen = () => {
   return (
     <View>
-      <Text></Text>
+      <Text> asdasdas</Text>
     </View>
   );
 };
